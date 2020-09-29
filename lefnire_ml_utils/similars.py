41d565017e0c0ce2df4eb7fa7dc05593ff1b4201
Tyler Renelle <tylerrenelle@gmail.com>
2020-09-28 18:58:12 -0700

    cleanup & comments

--- lefnire_ml_utils/similars.py
+++ lefnire_ml_utils/similars.py
@@ -9,26 +9,50 @@ from typing import List, Union
 
 
 # https://stackoverflow.com/a/7590709/362790
-def chain(device_in=None, device_out=None):
+def chain(device_in=None):
+    """
+    Decorator for chaining methods in Similars like Similars(x, y).embed().normalize().cosine()
+    When you want the output at any step, call .value(). It will retain its intermediate step
+    so you can continue chaining later, and call subsequent .value()
+    :param device_in: gpu|cpu|None. What device does this chain-step expect its values from?
+    """
     def decorator(fn):
         def wrapper(self, *args, **kwargs):
+            # Place x,y on device this chain method expects
             x, y = self.get_values(device_in)
-            self.device_out = device_out
             res = fn(self, x, y, *args, **kwargs)
+            # Always maintain [x, y] for consistency
             if type(res) != list: res = [res, None]
+            # Save intermediate result
             self.result = res
+            # Return self, so chained methods can continue
             return self
         return wrapper
     return decorator
 
 
 class Similars(object):
-    def __init__(self):
-        self.result = [None, None]
-        self.device_out = None
+    """
+    Various similarity helper functions.
+
+    * NLP methods: clean_text, tf_idf, embed (via sentence_transformers), etc
+    Call like Similars(sentences).embed() or Similars(lhs, rhs).clean_text().tfidf()
+
+    * Similarity methods: normalize, cosine, kmeans, agglomorative, etc
+    Call like Similars(x, y).noramlize().cosine().agglomorative()
+
+    Takes x, y. If y is provided, then we're comparing x to y. If y is None, then operations
+    are pairwise on x (x compared to x).
+    """
+    def __init__(
+        self,
+        x: Union[List[str], np.ndarray],
+        y: Union[List[str], np.ndarray] = None
+    ):
+        self.result = [x, y]
 
     def value(self):
-        x, y = self.get_values(self.device_out)
+        x, y = self.get_values('cpu')
         if y is None: return x
         return [x, y]
 
@@ -39,33 +63,33 @@ class Similars(object):
                 x = torch.tensor(x)
             if y is not None and not torch.is_tensor(y):
                 y = torch.tensor(y)
-            return [x, y]
-        if device == 'cpu':
+        elif device == 'cpu':
             if torch.is_tensor(x):
-                x = x.cpu()
+                x = x.cpu().numpy()
             if y is not None and torch.is_tensor(y):
-                y = y.cpu()
-            return [x, y]
+                y = y.cpu().numpy()
         return [x, y]
 
-    def start(
-        self,
-        x: Union[List[str], np.array],
-        y: Union[List[str], np.array]=None
-    ):
-        self.result = [x, y]
-        return self
+    def _join(self, x, y):
+        if y is None: return x
+        if type(x) == list: return x + y
+        if type(x) == np.ndarray: return np.vstack([x, y])
+        if torch.is_tensor(x): return torch.cat((x, y), 0)
+
+    def _split(self, joined, x, y):
+        if y is None: return [joined, None]
+        at = len(x) if type(x) == list else x.shape[0]
+        return [joined[:at], joined[at:]]
 
     @chain()
     def embed(self, x: List[str], y: List[str]=None):
-        sentences = x if y is None else x + y
-        m = SentenceTransformer('roberta-base-nli-stsb-mean-tokens')
-        enco = m.encode(sentences, batch_size=32, show_progress_bar=True, convert_to_tensor=True)
-        if y is None:
-            x = enco
-        else:
-            x, y = enco[:len(x)], enco[len(x):]
-        return [x, y]
+        enco = SentenceTransformer('roberta-base-nli-stsb-mean-tokens').encode(
+            self._join(x, y),
+            batch_size=32,
+            show_progress_bar=True,
+            convert_to_tensor=True
+        )
+        return self._split(enco, x, y)
 
     @chain()
     def dim_reduce(self, x, y): pass
@@ -73,24 +97,23 @@ class Similars(object):
     @chain()
     def tf_idf(self, x, y): pass
 
-    @chain(device_in='gpu', device_out='cpu')
+    @chain(device_in='gpu')
     def normalize(self, x, y):
-        norm = x if y is None else torch.cat((x, y), 0)
+        norm = self._join(x, y)
         norm = norm / norm.norm(dim=1)[:, None]
-        if y is None:
-            x = norm
-        else:
-            x, y = norm[:x.shape[0]], norm[x.shape[0]:]
-        return [x, y]
+        return self._split(norm, x, y)
 
-    @chain(device_in='gpu', device_out='cpu')
+    @chain(device_in='gpu')
     def cosine(self, x, y, abs=False):
+        """
+        :param abs: Hierarchical clustering wants [0 1], and dists.sort_by(0->1), but cosine is [-1 1]. Set True to
+            ensure cosine>0. Only needed currently for agglomorative()
+        """
         if y is None:
             y = x
         sim = torch.mm(x, y.T)
 
         if abs:
-            # Hierarchical clustering wants [0 1], and dists.sort_by(0->1), but cosine is [-1 1]
             # print("sim.min=", sim.min(), "sim.max=", sim.max())
             dist = (sim - 1).abs()
             # See https://stackoverflow.com/a/63532174/362790 for other options
@@ -103,39 +126,40 @@ class Similars(object):
     def _default_n_clusters(self, x):
         return math.floor(1 + 3.5 * math.log10(x.shape[0]))
 
-    @chain(device_in='gpu')
-    def agglomorative(self, x, y):
-        if y is not None:
-            self.result = [torch.cat((x, y), 0), None]
-        self.cosine(abs=True)
-        all = self.result[0].cpu()
-        nc = self._default_n_clusters(all)
+    # Don't set device_in, in case cluster_both=True and we're getting gpu (if False, it's cpu)
+    @chain()
+    def agglomorative(self, x, y, cluster_both=False):
+        """
+        Agglomorative (hierarchical) clustering.
+        :param cluster_both: if True, cluster x & y from the same pool & return [x_labels, y_labels]; otherwise just
+            cluster x. Must use cluster(combo_pairwise=True) beforehand.
+        """
+        x_, y_ = x, y
+        if cluster_both and y is not None:
+            self.result = [self._join(x, y), None]
+            self.cosine(abs=True)
+        x, y = self.get_values('cpu')
+        nc = self._default_n_clusters(x)
         labels = AgglomerativeClustering(
             n_clusters=nc,
             affinity='precomputed',
             linkage='average'
-        ).fit_predict(all)
-        if y is not None:
-            return [labels[:x.shape[0]], labels[x.shape[0]:]]
-        return [labels, None]
+        ).fit_predict(x)
+        return self._split(labels, x_, y_) if cluster_both else labels
 
     @chain(device_in='cpu')
-    def kmeans(self, x, y):
-        all = x
-        if y is not None:
-            all = np.vstack([x, y])
+    def kmeans(self, x, y, cluster_both=False):
+        combo = self._join(x, y) if cluster_both else x
 
         # Code from https://github.com/arvkevi/kneed/blob/master/notebooks/decreasing_function_walkthrough.ipynb
         step = 2  # math.ceil(guess.max / 10)
         K = range(2, 40, step)
-        inertias = []
+        scores = []
         for k in K:
-            km = KMeans(n_clusters=k).fit(all)
-            inertias.append(km.inertia_)
+            km = KMeans(n_clusters=k).fit(combo)
+            scores.append(km.inertia_)
         S = 1  # math.floor(math.log(all.shape[0]))  # 1=default; 100entries->S=2, 8k->3
-        kn = KneeLocator(list(K), inertias, S=S, curve='convex', direction='decreasing', interp_method='polynomial')
-        nc = kn.knee or self._default_n_clusters(all)
-        labels = KMeans(n_clusters=nc).fit(all).labels_
-        if y is not None:
-            return [labels[:x.shape[0]], labels[x.shape[0]:]]
-        return [labels, None]
+        kn = KneeLocator(list(K), scores, S=S, curve='convex', direction='decreasing', interp_method='polynomial')
+        nc = kn.knee or self._default_n_clusters(combo)
+        labels = KMeans(n_clusters=nc).fit(combo).labels_
+        return self._split(labels, x, y) if cluster_both else labels
--- tests/test_similars.py
+++ tests/test_similars.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from lefnire_ml_utils import Similars
 
 corpus = ['A man is eating food.',
@@ -14,17 +15,72 @@ corpus = ['A man is eating food.',
           'A cheetah chases prey on across a field.'
           ] * 5
 
-def test_x():
-    s = Similars()
-    res = s.start(corpus).embed().normalize().agglomorative()
-    print(res.value())
-    res = s.start(corpus).embed().normalize().kmeans()
-    print(res.value())
+split_ = len(corpus)//3
+X, Y = corpus[:split_], corpus[split_:]
 
-def test_x_y_agg():
-    s = Similars()
-    x, y = corpus[:6], corpus[6:]
-    res = s.start(x, y).embed().normalize().agglomorative()
-    print(res.value())
-    res = s.start(x, y).embed().normalize().kmeans()
-    print(res.value())
+
+@pytest.mark.parametrize(
+    "chain_step,x,y",
+    [
+        (1, corpus, None),
+        (1, X, Y),
+        (2, corpus, None),
+        (2, X, Y),
+        (3, corpus, None),
+        (3, X, Y)
+    ])
+def test_similars(chain_step, x, y):
+    res = Similars(x, y).embed()
+    if chain_step > 1: res = res.normalize()
+    if chain_step > 2: res = res.cosine()
+    res = res.value()
+    if chain_step == 3: return  # will test normalize below
+    if y is None:
+        res = [res]
+    else:
+        assert len(res) == 2
+    for r in res:
+        assert type(r) == np.ndarray
+        assert r.shape[1] == 768
+
+@pytest.mark.parametrize(
+    "abs,x,y",
+    [
+        (True, corpus, None),
+        (True, X, Y),
+        (False, corpus, None),
+        (False, X, Y),
+    ])
+def test_normalize(abs, x, y):
+    res = Similars(x, y).embed().normalize().cosine(abs=abs).value()
+    assert type(res) == np.ndarray
+    if abs:
+        all_pos = (res > 0-1e-15).all()
+        assert all_pos
+
+
+@pytest.mark.parametrize(
+    "algo,cluster_both,x,y",
+    [
+        ('kmeans', True, corpus, None),
+        ('kmeans', True, X, Y),
+        ('kmeans', False, corpus, None),
+        ('kmeans', False, X, Y),
+        ('agglomorative', True, corpus, None),
+        ('agglomorative', True, X, Y),
+        ('agglomorative', False, corpus, None),
+        ('agglomorative', False, X, Y),
+    ])
+def test_cluster(algo, cluster_both, x, y):
+    res = Similars(x, y).embed().normalize()
+    if algo == 'agglomorative':
+        res = res.agglomorative(cluster_both=cluster_both)
+    else:
+        res = res.kmeans(cluster_both=cluster_both)
+    res = res.value()
+    print(res)
+    if y is None:
+        assert len(res) == len(x)
+    else:
+        assert len(res) == 2
+        assert len(res[1]) == len(y)
